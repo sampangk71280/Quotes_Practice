6d7e0838da8234bc11defddd2eed3d83bdb58d9b
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2021-08-02 10:05:10 +1200

    index setup part 1

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 'On');  // change to 'Off' when live
+
+define ("DB_NAME","sampangk71280_L3_Quote"); 
+define ("DB_USERNAME","_sampangk71280");
+define ("DB_PASSWORD","QANbMDYSVME7gpyR"); 
+define ("DB_HOST","localhost");
+
+?>
