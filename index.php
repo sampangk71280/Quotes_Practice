61d4fab0eff2a13a28cedef7ada00d8e7841e059
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2021-08-09 10:05:14 +1200

    show all part 1 1:55

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <!DOCTYPE HTML>
 
+<?php 
+
+session_start();
+include("config.php");
+include("functions.php");
+
+// connect to database
+$dbconnect=mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD, DB_NAME);
+
+if(mysqli_connect_errno()){
+    echo "Connection failed:".mysqli_connect_error();
+    exit;
+}
+
+?>
+
+
 <html lang="en">
 
 <?php include("content/head.php"); ?> 
